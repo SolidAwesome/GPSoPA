58658e1d062057d6178b76a6e00c709aaca98a65
dorasakic5 <dorasakic66@gmail.com>
2026-02-20 14:22:00 +0000

    drop

--- db.sql
+++ db.sql
@@ -2,27 +2,25 @@
 CREATE DATABASE sustainable_donation;
 CREATE SCHEMA IF NOT EXISTS donation;
 SET search_path TO donation;
+DROP SCHEMA IF EXISTS donation CASCADE;
+CREATE SCHEMA donation;  
 CREATE EXTENSION IF NOT EXISTS postgis;
 
-DROP TABLE IF EXISTS units;
 CREATE TABLE units (
     unitid SERIAL PRIMARY KEY,
     unitname VARCHAR(20) NOT NULL UNIQUE
 );
 
-DROP TABLE IF EXISTS target_groups;
 CREATE TABLE target_groups (
     targetgroupid SERIAL PRIMARY KEY,
     groupname VARCHAR(50) NOT NULL UNIQUE
 );
 
-DROP TABLE IF EXISTS categories;
 CREATE TABLE categories (
     categoryid SERIAL PRIMARY KEY,
     categoryname VARCHAR(100) NOT NULL UNIQUE
 );
 
-DROP TABLE IF EXISTS subcategories;
 CREATE TABLE subcategories (
     subcategoryid SERIAL PRIMARY KEY,
     subcategoryname VARCHAR(300) NOT NULL,
@@ -30,29 +28,24 @@ CREATE TABLE subcategories (
     UNIQUE (subcategoryname, categoryid)
 );
 
-DROP TABLE IF EXISTS donation_items;
 CREATE TABLE donation_items (
     itemid SERIAL PRIMARY KEY,
-   -- categoryid INT REFERENCES categories(categoryid),
     subcategoryid INT NOT NULL REFERENCES subcategories(subcategoryid) ON DELETE RESTRICT,
     unitid INT NOT NULL REFERENCES units(unitid) ON DELETE RESTRICT,
-    targetgroupid INT NOT NULL REFERENCES target_groups(targetgroupid) ON DELETE SET NULL,
+    targetgroupid INT NOT NULL REFERENCES target_groups(targetgroupid) ON DELETE RESTRICT,
     UNIQUE (subcategoryid, targetgroupid)
 );
 
-DROP TABLE IF EXISTS donation_status;
 CREATE TABLE donation_status (
     statusid SERIAL PRIMARY KEY,
     donationstatus VARCHAR(50) NOT NULL UNIQUE
 );
 
-DROP TABLE IF EXISTS usertype;
 CREATE TABLE usertype (
     usertypeid SERIAL PRIMARY KEY,
     userrole VARCHAR(20) NOT NULL UNIQUE
 );
 
-DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     userid SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
@@ -61,7 +54,6 @@ CREATE TABLE users (
     created DATE DEFAULT NOW()
 );
 
-DROP TABLE IF EXISTS ngo;
 CREATE TABLE ngo (
     ngoid SERIAL PRIMARY KEY,
     ngoname VARCHAR(150) NOT NULL UNIQUE,
@@ -69,7 +61,6 @@ CREATE TABLE ngo (
     contact VARCHAR(100) NOT NULL UNIQUE
 );
 
-DROP TABLE IF EXISTS donation_centers;
 CREATE TABLE donation_centers (
     centerid SERIAL PRIMARY KEY,
     centername VARCHAR(150) NOT NULL,
@@ -79,7 +70,6 @@ CREATE TABLE donation_centers (
     geolocation GEOMETRY(POINT,4326) NOT NULL
 );
 
-DROP TABLE IF EXISTS ngo_center;
 CREATE TABLE ngo_center (
     ngocenterid SERIAL PRIMARY KEY,
     ngoid INT NOT NULL REFERENCES ngo(ngoid) ON DELETE CASCADE,
@@ -87,7 +77,6 @@ CREATE TABLE ngo_center (
     UNIQUE (ngoid, centerid)
 );
 
-DROP TABLE IF EXISTS events;
 CREATE TABLE events (
     eventid SERIAL PRIMARY KEY,
     eventname VARCHAR(150) NOT NULL,
@@ -99,20 +88,18 @@ CREATE TABLE events (
     quantitytarget INT NOT NULL CHECK (quantitytarget >= 0)
 );
 
-DROP TABLE IF EXISTS sizes;
 CREATE TABLE sizes (
     sizeid SERIAL PRIMARY KEY,
     productsize VARCHAR(10) NOT NULL UNIQUE
 );
 
-DROP TABLE IF EXISTS donations CASCADE;
 CREATE TABLE donations (
     id SERIAL PRIMARY KEY,
     userid INT NOT NULL REFERENCES users(userid),
     ngocenterid INT NOT NULL REFERENCES ngo_center(ngocenterid),
     itemid INT NOT NULL REFERENCES donation_items(itemid),
     sizeid INT REFERENCES sizes(sizeid),
-    quantity INT CHECK (quantity > 0),
+    quantity INT NOT NULL CHECK (quantity > 0),
     donationdescription VARCHAR(150),
     eventid INT REFERENCES events(eventid),
     statusid INT NOT NULL REFERENCES donation_status(statusid),
